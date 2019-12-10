6f0ca501c5fa6f83fcce742a29e00dd0447f9230
eumis <misievich@gmail.com>
2019-12-10 18:39:34 +0300

    added imports to package

--- injectool/__init__.py
+++ injectool/__init__.py
@@ -1,8 +1,9 @@
 """Dependency injection tool"""
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
-from .core import DependencyError, Resolver, Container, resolve
+from .core import DependencyError, Resolver, Container
+from .core import get_dependency_key, set_container, get_container, resolve, use_container
 from .resolvers import SingletonResolver, FunctionResolver
 from .resolvers import add_resolver, add_singleton, add_function_resolver, add_type
 from .injection import inject, dependency
