56b684b8de5ac2624b9dd7133125116163a6f794
eumis <misievich@gmail.com>
2019-12-10 18:21:28 +0300

    2.0.0 version

--- injectool/__init__.py
+++ injectool/__init__.py
@@ -1,8 +1,8 @@
 """Dependency injection tool"""
 
-__version__ = '1.1.2'
+__version__ = '2.0.0'
 
-from .core import DependencyError, Resolver, Container
+from .core import DependencyError, Resolver, Container, resolve
 from .resolvers import SingletonResolver, FunctionResolver
 from .resolvers import add_resolver, add_singleton, add_function_resolver, add_type
 from .injection import inject, dependency
