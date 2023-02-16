896c1131e6e0f6eddd871ea9baaf99088dadbdc3
eumis <eumisdev@proton.me>
2023-02-16 10:05:46 +0100

    removed SingletonResolver

--- injectool/resolvers.py
+++ injectool/resolvers.py
@@ -5,19 +5,9 @@ from typing import Any, Type
 from injectool.core import get_container
 
 
-class SingletonResolver:
-    """Singleton resolver"""
-
-    def __init__(self, value: Any):
-        self._value = value
-
-    def resolve(self):
-        return self._value
-
-
 def add_singleton(dependency: Any, value: Any):
     """Adds singleton value to current container"""
-    get_container().set(dependency, SingletonResolver(value).resolve)
+    get_container().set(dependency, lambda: value)
 
 
 def add_type(dependency: Any, type_: Type):
