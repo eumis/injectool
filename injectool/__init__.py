f88feda4d5b3272aec798d0e3e8ce9bda4e4551e
eumis <eumisdev@proton.me>
2023-02-22 17:14:30 +0100

    added resolvers imports

--- injectool/__init__.py
+++ injectool/__init__.py
@@ -4,5 +4,5 @@ __version__ = '2.0.4'
 
 from .core import DependencyError, Container
 from .core import get_dependency_key, set_default_container, get_container, resolve, use_container
-from .resolvers import add_singleton, add_type
+from .resolvers import add_singleton, add_type, add_scoped, add_per_thread
 from .injection import inject, dependency
